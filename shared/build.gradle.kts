141d612dcc24231423aa5e4e8bde6ab825d597eb
Manel Martos <manuel.martos-roldan@team.bumble.com>
2023-09-14 14:55:08 +0200

    Upgrade JVM from 11 to 17

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -43,8 +43,8 @@ android {
         minSdk = 30
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
