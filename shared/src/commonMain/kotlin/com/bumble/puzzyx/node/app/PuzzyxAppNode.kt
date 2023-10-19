d9dfdbd1759515caa8d22d94d6430bb5f36cd733
Manel Martos <manuel.martos-roldan@team.bumble.com>
2023-10-19 18:07:57 +0200

    Update list of screens

--- shared/src/commonMain/kotlin/com/bumble/puzzyx/node/app/PuzzyxAppNode.kt
+++ shared/src/commonMain/kotlin/com/bumble/puzzyx/node/app/PuzzyxAppNode.kt
@@ -56,10 +56,10 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 
 private val screens = listOf(
-    StackedMessages,
     Puzzle1,
     CallToAction,
     StarFieldMessageBoard,
+    StackedMessages,
 )
 
 class PuzzyxAppNode(
