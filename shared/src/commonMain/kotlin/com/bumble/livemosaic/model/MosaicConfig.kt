7cd5e3e22f28cf4fd4967b07a99eb77d3613fbc4
andreykovalev <andrey.kovalev@team.bumble.com>
2023-10-25 17:17:59 +0100

    Update MosaicConfig

--- shared/src/commonMain/kotlin/com/bumble/livemosaic/model/MosaicConfig.kt
+++ shared/src/commonMain/kotlin/com/bumble/livemosaic/model/MosaicConfig.kt
@@ -7,9 +7,9 @@ enum class MosaicConfig(
     val rows: Int
 ) {
 
-    PUZZLE1("mosaic/1_front", "mosaic/1_back",12, 7),
-    PUZZLE2("mosaic/2_front", "mosaic/2_back", 12, 7),
-    PUZZLE3("mosaic/3_front", "mosaic/3_back",12, 7);
+    MOSAIC1("mosaic/1_front", "mosaic/1_back",12, 7),
+    MOSAIC2("mosaic/2_front", "mosaic/2_back", 12, 7),
+    MOSAIC3("mosaic/3_front", "mosaic/3_back",12, 7);
 
 
     val maxEntryCount: Int = columns * rows
