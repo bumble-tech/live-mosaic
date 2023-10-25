31b99030543aa3f70725126db271d6591e6315b6
Sergio Borne <sergio.borne@team.bumble.com>
2023-10-25 18:22:47 +0100

    Fix error message

--- shared/src/commonMain/kotlin/com/bumble/livemosaic/model/Entries.kt
+++ shared/src/commonMain/kotlin/com/bumble/livemosaic/model/Entries.kt
@@ -27,7 +27,7 @@ val mosaic2Entries = entries
     .also {
         if (it.size > MOSAIC2.maxEntryCount)
             error("Mosaic 2 is already filled up. Add your entry to another one!")
-        if (entries.filter { it.mosaic == MOSAIC1 }.size < MOSAIC1.maxEntryCount)
+        if (it.isNotEmpty() && entries.filter { it.mosaic == MOSAIC1 }.size < MOSAIC1.maxEntryCount)
             error("Mosaic 1 is not yet filled up. Add your entry to it!")
         if (it.map { it.githubUserName }.distinct().size < it.size) {
             error("One entry per mosaic is the limit, but you can try again in the next one!")
@@ -39,9 +39,9 @@ val mosaic3Entries = entries
     .also {
         if (it.size > MOSAIC3.maxEntryCount)
             error("Mosaic 3 is already filled up. Add your entry to another one!")
-        if (entries.filter { it.mosaic == MOSAIC1 }.size < MOSAIC1.maxEntryCount)
+        if (it.isNotEmpty() && entries.filter { it.mosaic == MOSAIC1 }.size > MOSAIC1.maxEntryCount)
             error("Mosaic 1 is not yet filled up. Add your entry to it!")
-        if (entries.filter { it.mosaic == MOSAIC2 }.size < MOSAIC2.maxEntryCount)
+        if (it.isNotEmpty() && entries.filter { it.mosaic == MOSAIC2 }.size < MOSAIC2.maxEntryCount)
             error("Mosaic 2 is not yet filled up. Add your entry to it!")
         if (it.map { it.githubUserName }.distinct().size < it.size) {
             error("One entry per mosaic is the limit, but you can try again in the next one!")
