c695581bbfb07935f75b8f9974fc41db946d370f
Kitty <57370965+mho-bbk@users.noreply.github.com>
2023-10-26 14:54:02 +0100

    Update Entries.kt
    Droidcon shoutout!

--- shared/src/commonMain/kotlin/com/bumble/livemosaic/model/Entries.kt
+++ shared/src/commonMain/kotlin/com/bumble/livemosaic/model/Entries.kt
@@ -223,6 +223,11 @@ val entries = listOf(
         githubUserName = "marekpdev",
         message = "Great to talk to so many interesting people at Droidcon!"
     ),
+    Entry.Text(
+        mosaic = MOSAIC1,
+        githubUserName = "mho-bbk",
+        message = "Amazing Droidcon! Very inspired by everyone here today :)"
+    ),
 )
 
 val mosaic1Entries = entries
