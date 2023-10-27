142700d972ee103449ad568a903e5cab63e5318a
AnujVashisht <anujsharma@live.com>
2023-10-27 11:28:42 +0100

    Update Entries.kt
    Anujvashisht

--- shared/src/commonMain/kotlin/com/bumble/livemosaic/model/Entries.kt
+++ shared/src/commonMain/kotlin/com/bumble/livemosaic/model/Entries.kt
@@ -136,6 +136,11 @@ val entries = listOf(
         githubUserName = "Phelenor",
         message = "Greetings from Sofascore!"
     ),
+    Entry.Text(
+        mosaic = MOSAIC1,
+        githubUserName = "AnujVashisht",
+        message = "This is the most popular entry, coming straight from phone 😅"
+    ),
     Entry.ComposableContent(
         mosaic = MOSAIC1,
         githubUserName = "adriantache",
