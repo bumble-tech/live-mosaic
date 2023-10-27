bee3cf86b9028117aef4a79322d25b6dfe4fbb11
Wisors <Wisors@users.noreply.github.com>
2023-10-27 12:21:00 +0100

    Fix dependency

--- shared/src/commonMain/kotlin/com/bumble/livemosaic/model/Entries.kt
+++ shared/src/commonMain/kotlin/com/bumble/livemosaic/model/Entries.kt
@@ -21,6 +21,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.bumble.livemosaic.participant.ClockWidget
 import com.bumble.livemosaic.participant.DroidconLondonHalloweenSpecial
+import com.bumble.livemosaic.participant.MagicButton
 
 val entries = listOf(
     Entry.Text(
